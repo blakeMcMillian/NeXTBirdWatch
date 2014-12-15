cf227940a86e50f39834b5ff0028f42dd37d927e
blakeMcMillian <b4mcmillian@gmail.com>
2014-12-15 13:51:56 -0500

    Added a Twitter Analyzer class into the application

--- nextBirdWatch/Analyzer.h
+++ nextBirdWatch/Analyzer.h
@@ -7,10 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "STTwitter.h"
+#import "tweetInstance.h"
+#import "Regexer.h"
 
 @interface Analyzer : NSObject
 
 //Testing Testing
 
+
+//tweetInstance
+@property (strong, nonatomic) tweetInstance *tweet;
+
+//STTwitter
+@property (nonatomic, strong) STTwitterAPI *twitter;
+
+//NSArray
+@property (strong, nonatomic) NSArray* searchResultsFromTwitter;
+@property (strong, nonatomic) NSArray* arrayOfTweetsFromTwitterSearch;
+
+//NSInteger
+@property int queryCountForRegexSearch;
+@property int tweetCountForStreaming;
+
+//BOOL
+@property BOOL regexIsEnabled;
+@property BOOL didCompleteRegexQuery;
 
 @end
--- nextBirdWatch/Analyzer.m
+++ nextBirdWatch/Analyzer.m
@@ -10,4 +10,101 @@
 
 @implementation Analyzer
 
+
+-(void)getSearchResultsFromTwitter
+{
+    STTwitterAPI *twitter = [STTwitterAPI twitterAPIOSWithFirstAccount];
+    
+    [twitter verifyCredentialsWithSuccessBlock:^(NSString *username)
+     {
+         [twitter getSearchTweetsWithQuery:@"" geocode:@"" lang:@"" locale:@"" resultType:@"recent" count:@"100" until:@"" sinceID:@"" maxID:@"" includeEntities:@(0) callback:@"" successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
+             
+             NSLog(@"searchMetadata = %@", searchMetadata);
+             NSLog(@"statuses = %@", statuses);
+             
+             //Storing the search results from Twitter
+             self.searchResultsFromTwitter = [[NSArray alloc]initWithArray:statuses copyItems:YES];
+             
+         } errorBlock:^(NSError *error)
+          {
+              NSLog(@"twitter error = %@", error);
+              
+          }];//end - error Block
+         
+     } errorBlock:^(NSError *error)
+     {
+         NSLog(@"-- error %@", error);
+     }];
+    
+}
+
+-(void)getStreamingResultsFromTwitter
+    {
+        STTwitterAPI *twitter = [STTwitterAPI twitterAPIOSWithFirstAccount];
+        
+        [twitter verifyCredentialsWithSuccessBlock:^(NSString *username)
+         {
+             NSMutableArray *data = [NSMutableArray array];
+             NSMutableArray *dataWithCorrectMath = [NSMutableArray array];
+             
+             
+             __block id twitterRequest = [twitter postStatusesFilterUserIDs:nil
+                                                            keywordsToTrack:nil
+                                                      locationBoundingBoxes:@[@"-180",@"-90",@"180",@"90"]
+                                                                  delimited:@20
+                                                              stallWarnings:nil
+                                                              progressBlock:^(NSDictionary *tweet)
+                                          {
+                                              
+                                              NSLog(@"-- data count: %lu", (unsigned long)[data count]);
+                                              
+                                              if (([data count] >= self.tweetCountForStreaming) || ([self.searchResultsFromTwitter count] == self.queryCountForRegexSearch))
+                                              {
+                                                  NSLog(@"-- cancel");
+                                                  self.didCompleteRegexQuery = YES;
+                                                  [twitterRequest cancel];
+                                              }
+                                              else
+                                              {
+                                                  //Lightly parsing the regular expression
+                                                  NSString * regex = @"";
+                                                  
+                                                  regex = [regex stringByReplacingOccurrencesOfString:@"\""
+                                                                                           withString:@"\\\""];
+                                                  
+                                                  BOOL match = [[tweet valueForKey:@"text"] rx_matchesPattern:regex options:NSRegularExpressionAllowCommentsAndWhitespace];
+                                                  
+                                                  
+                                                  
+                                                  //adding the item to data
+                                                  [data addObject:tweet];
+                                                  if (match)
+                                                  {
+                                                      [dataWithCorrectMath addObject:tweet];
+                                                      
+                                                      //Storing the search results from Twitter
+                                                      self.searchResultsFromTwitter = [[NSArray alloc]initWithArray:dataWithCorrectMath copyItems:YES];
+                                                      
+                                                  }//end - if statement
+                                                  
+                                                  
+                                                  NSLog(@"%@",[tweet valueForKey:@"text"]);
+                                              }
+                                              
+                                              
+                                          } stallWarningBlock:nil
+                                                                 errorBlock:^(NSError *error) {
+                                                                     NSLog(@"-- error 2: %@", error);
+                                                                 }];
+             
+         } errorBlock:^(NSError *error)
+         {
+             NSLog(@"-- error %@", error);
+         }];
+        
+    
+
+    
+}//end - getSearchResultsFromTwitter
+
 @end
